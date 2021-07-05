56eec3deb8e4d7b8b2eb9cd41f27d1389f64fee6
João Dias Pinheiro <NB27498@novabase.pt>
2021-07-05 10:50:58 +0100

    first commit

---
+++ src/test/java/com/celfocus/training/spaceover/serviceregistry/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.celfocus.training.spaceover.serviceregistry.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
